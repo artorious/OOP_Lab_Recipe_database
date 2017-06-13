c4988e4ebcea7ae2aa75661af37dbee95006e0e0
Arthur Ngondo <ngondoarthur@gmail.com>
2017-06-13 15:49:15 +0300

    Added - Print a single recipe

--- recipebook.py
+++ recipebook.py
@@ -30,13 +30,27 @@ class Cookbook():
             response = input('Enter a Selection (0 - 6) --> ')
 
             if response == '1': # Show all recipes
-                pass
+                cbk.PrintAllRecipes()
+                print('Total Recipes - {}'.format(cbk.totalcount))
+                print('-' * 70)
+                res = input('Press Return Key to Continue --> ')
 
             elif response == '2': # Search for a recipe
                 pass
 
             elif response == '3': # Show a single Recipe
-                pass
+                cbk.PrintAllRecipes()
+                try:
+                    res = int(input('Select a Recipe -> '))
+                    if res <= cbk.totalcount:
+                        cbk.PrintSingleRecipe(res)
+                    elif res == cbk.totalcount + 1:
+                        print('Back to Menu.....')
+                    else:
+                        print('Unrecognized Command. Returning to menu.')
+                except ValueError:
+                    print('Not a Number......Back to menu.')
+
 
             elif response == '4': # Delete Recipe
                 pass
@@ -53,7 +67,40 @@ class Cookbook():
 
             else:
                 print('Unrecognized Command. Try Again.')
-
+        
+    def PrintAllRecipes(self):
+        print('{0} {1} {2} {3}'.format('Item'.ljust(5), 'Name'.ljust(30), \
+        'Serves'.ljust(20), 'Source'.ljust(30)))
+        print('-' * 70)
+        sql = 'SELECT * FROM Recipes'
+        cntr = 0
+        for x in cursor.execute(sql):
+            cntr += 1
+            print('{0} {1} {2} {3} ' .format(str(x[0]).rjust(5), x[1].ljust(30), \
+                x[2].ljust(20), x[3].ljust(30)))
+            print('-' * 70)
+            self.totalcount = cntr
+    
+    def PrintSingleRecipe(self, which):
+        sql = 'SELECT * FROM Recipes WHERE pkID = {}'.format(str(which))
+        print('-' * 70)
+        for x in cursor.execute(sql):
+            recipeid = x[0]
+            print('Title: ' + x[1])
+            print('Serves: ' + x[2])
+            print('Source: ' + x[3])
+        print('-' * 70)
+        sql = 'SELECT * FROM Ingredients WHERE pkID = {}'.format(recipeid)
+        print('Ingredients List: ')
+        for x in cursor.execute(sql):
+            print(x[1])
+        print(' ')
+        print('Instructions: ')
+        sql = 'SELECT * FROM Instructions WHERE RecipeID = {}'.format(recipeid)
+        for x in cursor.execute(sql):
+            print(x[1])
+        print('-' * 70)
+        resp = input('Press return key to continue -> ')
     
 if __name__ == '__main__':
     Cookbook.Menu()
